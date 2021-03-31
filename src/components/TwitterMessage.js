56d3683781ffdabb3c9b4089b9bd03f016688e91
Ramsey Chapin <ramsey@ramseychapin.com>
2021-03-30 20:23:05 -0700

    commit tree 4cc5bddf997fd1daed610b33b02a9e65a5295b3e

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,34 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      tweet: "",
+    };
+  }
+
+  handleTweetChange = event => {
+    this.setState({
+      tweet: event.target.value
+    });
+  }
+
+  remainingCharacters = () => {
+    return this.props.maxChars - this.state.tweet.length
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input 
+
+          type="text"
+          name="message"
+          id="message"
+          onChange={event => this.handleTweetChange(event)}
+          value = {this.state.tweet}
+        />
+        <p>{this.remainingCharacters()} characters left </p>
       </div>
     );
   }
